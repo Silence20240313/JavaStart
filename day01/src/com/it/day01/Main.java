0409074bbcd16e731dc849ed646074ef4894f0af
Silence20240313 <liangxiaoli378@gmail.com>
2024-07-11 22:39:53 +0900

    day 01

---
+++ day01/src/com/it/day01/Main.java
@@ -0,0 +1,24 @@
+package com.it.day01;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        TODO  2024.7.11笔记（P1-P12）
+        1.快捷键：复制当前行 ctrl+D
+        2.变量：可以改变的向量存储
+        ①声明 ：数据类型 变量的名称   ②赋值：变量名称=数据
+         String username = "李四";
+        ③使用：直接访问变量的名称即可
+        System.out.println(name);
+        3.标识符：标识数据的符号（主要用于起名 eg:下面的name）
+                String name = "张三";
+        ①命名规则：
+        √  英文小写（单词） 符号（下划线 美元符号 ）  数字（0-9 不能放在开头）
+        ×  空格  关键字保留字   大括号范围内，标识符不允许重复
+        ※ 标识符区分大小写  驼峰标识userName
+
+
+                 */
+
+    }
+}
